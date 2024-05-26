ccc3ff28af577ae9e2cd83b5fba1668def5b77b1
Sekul <wojowniksek@gmail.com>
2024-05-26 03:39:31 +0200

    feat(apps/vue): register method in auth service

--- apps/vue/src/services/auth.ts
+++ apps/vue/src/services/auth.ts
@@ -1,18 +1,41 @@
 import { request } from "./instance";
 
+type AuthResponse = {
+    access_token: string;
+};
+
 export async function login(username: string, password: string) {
     try {
-        type Response = {
-            access_token: string;
-        };
-
-        const { data, success } = await request<Response>({
+        const { data, success } = await request<AuthResponse>({
             method: "POST",
             data: {
                 username,
                 password,
             },
             url: "/auth/login",
+        });
+        if (!success) {
+            return false;
+        }
+
+        window.localStorage.setItem("jwt", data.access_token);
+        return data;
+    } catch (e) {
+        return false;
+    }
+}
+
+export async function register(email: string, login: string, label: string, password: string) {
+    try {
+        const { data, success } = await request<AuthResponse>({
+            method: "POST",
+            data: {
+                email,
+                login,
+                label,
+                password,
+            },
+            url: "/auth/register",
         });
         if (!success) {
             return false;

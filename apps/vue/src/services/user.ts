3b6263ead0153716ecd6ddabbf1d966db194332b
Sekul <wojowniksek@gmail.com>
2024-05-30 00:56:35 +0200

    feat(apps/vue): user service

---
+++ apps/vue/src/services/user.ts
@@ -0,0 +1,65 @@
+import { UpdateMeDto } from "@repo/types";
+
+import { request } from "./instance";
+
+export async function getUser() {
+    try {
+        const { data, success } = await request({
+            url: "/user/@me",
+        });
+
+        if (!success) {
+            return false;
+        }
+        return data;
+    } catch (e) {
+        return false;
+    }
+}
+
+export async function getUserByUsername(username: string) {
+    try {
+        const { data, success } = await request({
+            url: `/user/${username}`,
+        });
+
+        if (!success) {
+            return false;
+        }
+        return data;
+    } catch (e) {
+        return false;
+    }
+}
+
+export async function searchUsers(input: string) {
+    try {
+        const { data, success } = await request({
+            url: `/user/search/${input}`,
+        });
+
+        if (!success) {
+            return false;
+        }
+        return data;
+    } catch (e) {
+        return false;
+    }
+}
+
+export async function updateUser(dto: UpdateMeDto) {
+    try {
+        const { data, success } = await request({
+            url: `/user/@me`,
+            method: "PUT",
+            data: dto,
+        });
+
+        if (!success) {
+            return false;
+        }
+        return data;
+    } catch (e) {
+        return false;
+    }
+}

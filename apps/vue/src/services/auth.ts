624cd8abd86ca232f6f081d031201951ffd83960
Sekul <wojowniksek@gmail.com>
2024-05-30 00:52:51 +0200

    feat(apps/vue): auth service

--- apps/vue/src/services/auth.ts
+++ apps/vue/src/services/auth.ts
@@ -4,6 +4,10 @@ type AuthResponse = {
     access_token: string;
 };
 
+export function logout() {
+    window.localStorage.removeItem("jwt");
+}
+
 export async function login(username: string, password: string) {
     try {
         const { data, success } = await request<AuthResponse>({
@@ -42,6 +46,40 @@ export async function register(email: string, login: string, label: string, pass
         }
 
         window.localStorage.setItem("jwt", data.access_token);
+        return data;
+    } catch (e) {
+        return false;
+    }
+}
+
+export async function forgotPassword(email: string) {
+    try {
+        const { data, success } = await request<{ id: string }>({
+            method: "POST",
+            data: { email },
+            url: "/auth/forgot-password",
+        });
+        if (!success) {
+            return false;
+        }
+        return data;
+    } catch (e) {
+        return false;
+    }
+}
+
+export async function changePassword(uid: string, password: string) {
+    try {
+        const { data, success } = await request({
+            method: "PUT",
+            data: {
+                password,
+            },
+            url: `/auth/change-password/${uid}`,
+        });
+        if (!success) {
+            return false;
+        }
         return data;
     } catch (e) {
         return false;

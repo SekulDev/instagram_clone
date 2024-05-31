b8d92d677305518eb6f5ed4a960f2e2ba8f8bf6f
Sekul <wojowniksek@gmail.com>
2024-05-31 22:52:23 +0200

    feat(apps/frontend): title component

---
+++ apps/frontend/src/components/Title.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { defineProps, onMounted, watch } from "vue";
+
+const DEFAULT_TITLE = "Instagram";
+
+const props = defineProps<{
+    title: string;
+}>();
+
+onMounted(() => {
+    document.title = DEFAULT_TITLE + " - " + props.title;
+});
+
+watch(
+    () => props.title,
+    (newTitle) => {
+        document.title = DEFAULT_TITLE + " - " + newTitle;
+    },
+);
+</script>

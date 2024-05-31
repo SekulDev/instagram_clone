9a9739bfd22757f824be723a380cc7045a57533f
Sekul <wojowniksek@gmail.com>
2024-05-31 18:31:34 +0200

    feat(apps/frontend): 404 page

--- apps/frontend/src/views/NotFound.vue
+++ apps/frontend/src/views/NotFound.vue
@@ -1,5 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
 
 <template>
-    <div></div>
+    <div class="container flex w-[100vh] flex-col items-center gap-7 p-5">
+        <h1 class="text-center text-2xl font-semibold">Przepraszamy, ta strona jest niedostępna</h1>
+        <h3 class="font-regular text-md text-center">
+            Kliknięty link mógł być uszkodzony lub strona mogła zostać usunięta.
+            <RouterLink :to="{ name: 'Home' }">Wróć do Instagramu.</RouterLink>
+        </h3>
+    </div>
 </template>

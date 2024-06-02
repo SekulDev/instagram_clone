a0844e5b600bd220bebd813abc1cc2acfd545c82
Sekul <wojowniksek@gmail.com>
2024-06-02 03:41:37 +0200

    feat(apps/frontend): add sheet to ui

---
+++ apps/frontend/src/components/ui/sheet/SheetContent.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import { cn } from "@/lib/utils";
+import { X } from "lucide-vue-next";
+import {
+    DialogClose,
+    DialogContent,
+    type DialogContentEmits,
+    type DialogContentProps,
+    DialogOverlay,
+    DialogPortal,
+    useForwardPropsEmits,
+} from "radix-vue";
+import { type HTMLAttributes, computed } from "vue";
+
+import { type SheetVariants, sheetVariants } from ".";
+
+interface SheetContentProps extends DialogContentProps {
+    class?: HTMLAttributes["class"];
+    side?: SheetVariants["side"];
+}
+
+defineOptions({
+    inheritAttrs: false,
+});
+
+const props = defineProps<SheetContentProps>();
+
+const emits = defineEmits<DialogContentEmits>();
+
+const delegatedProps = computed(() => {
+    const { class: _, side, ...delegated } = props;
+
+    return delegated;
+});
+
+const forwarded = useForwardPropsEmits(delegatedProps, emits);
+</script>
+
+<template>
+    <DialogPortal>
+        <DialogOverlay
+            class="data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50"
+        />
+        <DialogContent :class="cn(sheetVariants({ side }), props.class)" v-bind="{ ...forwarded, ...$attrs }">
+            <slot />
+
+            <DialogClose
+                class="ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute right-4 top-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none"
+            >
+                <X class="text-muted-foreground h-4 w-4" />
+            </DialogClose>
+        </DialogContent>
+    </DialogPortal>
+</template>

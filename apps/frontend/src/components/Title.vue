3bcb17f512a90d38e1af51d0191c33c32777e739
Sekul <wojowniksek@gmail.com>
2024-06-02 02:21:40 +0200

    refactor(apps/frontend): title component optional props

--- apps/frontend/src/components/Title.vue
+++ apps/frontend/src/components/Title.vue
@@ -4,16 +4,24 @@ import { defineProps, onMounted, watch } from "vue";
 const DEFAULT_TITLE = "Instagram";
 
 const props = defineProps<{
-    title: string;
+    title?: string;
 }>();
 
 onMounted(() => {
+    if (!props.title) {
+        document.title = DEFAULT_TITLE;
+        return;
+    }
     document.title = DEFAULT_TITLE + " - " + props.title;
 });
 
 watch(
     () => props.title,
     (newTitle) => {
+        if (!props.title) {
+            document.title = DEFAULT_TITLE;
+            return;
+        }
         document.title = DEFAULT_TITLE + " - " + newTitle;
     },
 );

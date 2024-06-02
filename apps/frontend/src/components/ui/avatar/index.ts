ee8caa30c850a78f34218fbb779a28f68dfbf0ab
Sekul <wojowniksek@gmail.com>
2024-06-02 02:22:32 +0200

    tweak(apps/frontend): avatar sizes

--- apps/frontend/src/components/ui/avatar/index.ts
+++ apps/frontend/src/components/ui/avatar/index.ts
@@ -11,7 +11,7 @@ export const avatarVariant = cva(
             size: {
                 sm: "h-6 w-6 text-xs",
                 base: "h-16 w-16 text-2xl",
-                lg: "h-32 w-32 text-5xl",
+                lg: "h-36 w-36 text-5xl",
             },
             shape: {
                 circle: "rounded-full",

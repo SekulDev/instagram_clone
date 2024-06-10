75716a4c4a99a0340789451ee1e6ed6c60a124e3
Sekul <wojowniksek@gmail.com>
2024-06-10 23:07:18 +0200

    feat(apps/frontend): add command to components ui

---
+++ apps/frontend/src/components/ui/command/CommandGroup.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { cn } from "@/lib/utils";
+import type { ComboboxGroupProps } from "radix-vue";
+import { ComboboxGroup, ComboboxLabel } from "radix-vue";
+import { type HTMLAttributes, computed } from "vue";
+
+const props = defineProps<
+    ComboboxGroupProps & {
+        class?: HTMLAttributes["class"];
+        heading?: string;
+    }
+>();
+
+const delegatedProps = computed(() => {
+    const { class: _, ...delegated } = props;
+
+    return delegated;
+});
+</script>
+
+<template>
+    <ComboboxGroup
+        v-bind="delegatedProps"
+        :class="
+            cn(
+                'text-foreground [&_[cmdk-group-heading]]:text-secondary-foreground overflow-hidden p-1 [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:py-1.5 [&_[cmdk-group-heading]]:text-xs [&_[cmdk-group-heading]]:font-medium',
+                props.class,
+            )
+        "
+    >
+        <ComboboxLabel v-if="heading" class="text-muted-foreground px-2 py-1.5 text-xs font-medium">
+            {{ heading }}
+        </ComboboxLabel>
+        <slot />
+    </ComboboxGroup>
+</template>
